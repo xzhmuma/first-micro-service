100b88df31a57a40a9f8958522f5d2671c1cd63a
chuchen <xzhmuma@qq.com>
2017-06-15 11:12:48 +0800

    初始版

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # first-micro-service
-##springcloud 微服务开发实践   会添加一个个 比较有意思有用的微服务上去.
+springcloud 微服务开发实践   会添加一个个 比较有意思有用的微服务上去.
 
 
-###注册中心         eureka-server
-###网关             zuul-server
-###权限             auth-server
-###文件服务器        dfs-server
-###配置服务器        config-server
+包含以下微服务.：
+* 注册中心         eureka-server
+* 网关             zuul-server
+* 权限             auth-server
+* 文件服务器        dfs-server
+* 配置服务器        config-server
--- dfs-server/src/main/java/com/cloud/chuchen/DfsServerApplication.java
+++ dfs-server/src/main/java/com/cloud/chuchen/DfsServerApplication.java
@@ -3,12 +3,14 @@ package com.cloud.chuchen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class FastdfsServerApplication {
+@EnableHystrix
+public class DfsServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FastdfsServerApplication.class, args);
+		SpringApplication.run(DfsServerApplication.class, args);
 	}
 }
