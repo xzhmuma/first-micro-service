c1da8c092712faf823b785db02159534f263e0f7
chuchen <xzhmuma@qq.com>
2017-07-10 14:12:37 +0800

    初始版

--- hystrix-server/src/main/docker/start_eureka.sh
+++
@@ -1,3 +0,0 @@
-nohup java -jar /opt/service/springcloud/eureka/eureka-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer1 command 1>/dev/null 2>/dev/null &
-nohup java -jar /opt/service/springcloud/eureka/eureka-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer2 command 1>/dev/null 2>/dev/null &
-nohup java -jar /opt/service/springcloud/eureka/eureka-server-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer3 command 1>/dev/null 2>/dev/null &
